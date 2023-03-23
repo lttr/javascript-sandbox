65bd4c2415b397b644e0a55e4a6d24188c4696dd
Lukas Trumm <lukas.trumm@gmail.com>
2023-03-23 14:50:13 +0100

    Add datetime utils

---
+++ datetime-display-names.js
@@ -0,0 +1,25 @@
+const LOCALE = "cs";
+
+function namesOfWeekdays() {
+  return Array.from(Array(7), (_, i) =>
+    new Date(0, i).toLocaleDateString(LOCALE, { weekday: "long" })
+  );
+}
+
+function namesOfMonths() {
+  return Array.from(Array(12), (_, i) =>
+    new Date(0, i).toLocaleDateString(LOCALE, { month: "long" })
+  );
+}
+
+function localizeDateTimeField(field) {
+  return new Intl.DisplayNames(LOCALE, { type: "dateTimeField" }).of(field);
+}
+
+console.log(namesOfWeekdays());
+
+console.log(namesOfMonths());
+
+console.log(
+  ["minute", "hour", "day", "month", "year"].map(localizeDateTimeField)
+);
